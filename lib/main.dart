e02fda75dd25f263397c927ff5b3240f6b83276f
Jesús Guerrero García <jesusguerrero9515@gmail.com>
2021-11-10 11:30:19 -0600

    cambio de apellido

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Galaspace',
       home: Scaffold(
         body: Center(
-          child: Text('Hola Mundo Miguel!'),
+          child: Text('Hola Mundo Miguel dueñez!'),
         ),
       ),
     );
