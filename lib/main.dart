20eb975b09c07a56f11e7591d35df6dd23f0ef93
Jesús Guerrero García <jesusguerrero9515@gmail.com>
2021-11-10 11:10:01 -0600

    cambio de hola mundo

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Galaspace',
       home: Scaffold(
         body: Center(
-          child: Text('Hola Mundo!'),
+          child: Text('Hola Mundo Miguel!'),
         ),
       ),
     );
