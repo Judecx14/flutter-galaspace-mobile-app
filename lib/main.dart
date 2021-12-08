5e243f1aaaf30061aa1d57034746d69417c18910
Cesar Hernandez <cesarjaviersanchhdz@gmail.com>
2021-12-08 13:42:02 -0600

    modified

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         title: 'Componentes App',
         debugShowCheckedModeBanner: false,
         //home: HomePage(),
-        initialRoute: 'Login',
+        initialRoute: 'Messages',
         routes: getRoutes(),
         /*onGenerateRoute: (setting) {
           print('Ruta llamda: ${setting.name}');
